b6108ddfc2415c6eaf95529c73460190b39378ab
Edvard Høiby <edvardhoiby@outlook.com>
2023-12-21 12:52:17 +0100

    fix: host ip

--- src/index.ts
+++ src/index.ts
@@ -35,7 +35,7 @@ fastify.get('/health', async (_request, reply) => {
 
 (async () => {
   try {
-    await fastify.listen({ port: 3000 });
+    await fastify.listen({ host: '0.0.0.0', port: 3000 });
     console.log(`server listening on ${3000}`);
   } catch (err) {
     fastify.log.error(err);
